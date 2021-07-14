ace0c161cc947ec919d20d120fe75dad6a971fce
Noah Gardner <ngngardner@gmail.com>
2021-07-14 14:55:48 -0400

    A new start

---
+++ README.md
@@ -0,0 +1,3 @@
+# resume
+
+https://ngngardner.github.io/resume/
---
+++ shell.nix
@@ -0,0 +1,11 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
+in
+with pkgs;
+pkgs.mkShell {
+  buildInputs = [
+    # tex
+    texlive.combined.scheme-full
+  ];
+}
