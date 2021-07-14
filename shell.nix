c36a84bc2dfe65c67652d0bf118f2baf9484843b
Noah Gardner <ngngardner@gmail.com>
2021-07-14 14:23:17 -0400

    drop elm

--- shell.nix
+++ shell.nix
@@ -5,20 +5,11 @@ in
 with pkgs;
 pkgs.mkShell {
   buildInputs = [
-    # nix
-    elm2nix
-    
     # tex
     texlive.combined.scheme-full
 
     # node
     nodejs
     nodePackages.npm
-
-    # elm
-    elmPackages.elm
-    elmPackages.elm-test
-    elmPackages.elm-format
-    elmPackages.elm-live
   ];
 }
