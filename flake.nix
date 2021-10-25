d2012bdde341c34fbcaa7c61721fdfd7ff08e52b
Noah Gardner <ngngardner@gmail.com>
2021-10-25 14:36:32 -0400

    updates

--- flake.nix
+++ flake.nix
@@ -2,16 +2,36 @@
   description = "Resume";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-21.05";
-    flake-utils = { url = "github:numtide/flake-utils"; };
+    nixpkgs = { url = "github:nixos/nixpkgs/d189bf92f9be23f9b0f6c444f6ae29351bb7125c"; };
+    utils = { url = "github:numtide/flake-utils"; };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShell = import ./shell.nix { inherit pkgs; };
-        }
-      );
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell
+          {
+            packages = [
+              # tex dev
+              (pkgs.texlive.combine {
+                inherit (pkgs.texlive)
+                  scheme-medium
+                  latexmk
+                  latexindent
+
+                  preprint
+                  titlesec
+                  enumitem
+                  fontawesome
+                  ;
+              })
+            ];
+          };
+      }
+    );
 }
--- shell.nix
+++ shell.nix
@@ -1,17 +1,3 @@
-{ pkgs }:
-pkgs.mkShell {
-  packages = [
-    # tex dev
-    (pkgs.texlive.combine {
-      inherit (pkgs.texlive)
-        scheme-small
-        ;
-    })
-
-    # html dev
-    pkgs.nodePackages.http-server
-
-    # nix dev
-    pkgs.nixpkgs-fmt
-  ];
-}
+(import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
+  src = ./.;
+}).shellNix
